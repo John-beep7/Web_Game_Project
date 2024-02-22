3ae4c6b848b790fe7af35c295f0d08248b7208fa
Tomasz Skrzyp <stomasz296@gmail.com>
2024-02-22 20:52:57 +0100

    gg

--- the game/core/views.py
+++ the game/core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from item.models import Category, scenario
 
 def index(request):
-    return render(request, 'core/index.html')
+    scenarios = scenario.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'core/index.html',{
+        'categories':categories,
+        'scenarios':scenarios,
+    })
 def contact(request):
     return render(request, 'core/contact.html')
--- the game/item/models.py
+++ the game/item/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User 
+
 
 class Category(models.Model):
     name=models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = 'Categories'
+    def __str__(self):
+        return self.name
+class scenario(models.Model):
+    category=models.ForeignKey(Category, related_name='scenarios', on_delete=models.CASCADE)
+    name=models.CharField(max_length=255)
+    description=models.TextField()
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    #possible optins for the scenario
+    created_by=models.ForeignKey(User,related_name='scenarios',on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    image=models.ImageField(upload_to='scenario_images',blank=True,null=True)
+    def __str__(self):
+        return self.name
